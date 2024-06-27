1eb1a2cc1ef682fd765e872db1f4ec9a5b27502f
Noga Avraham <noga001@gmail.com>
2024-06-27 18:11:18 +0300

    Initial project add

---
+++ src/main/java/guru/springframework/My_SFG_Pet_Clinic_2/MySfgPetClinic2Application.java
@@ -0,0 +1,13 @@
+package guru.springframework.My_SFG_Pet_Clinic_2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MySfgPetClinic2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MySfgPetClinic2Application.class, args);
+	}
+
+}
---
+++ src/test/java/guru/springframework/My_SFG_Pet_Clinic_2/MySfgPetClinic2ApplicationTests.java
@@ -0,0 +1,13 @@
+package guru.springframework.My_SFG_Pet_Clinic_2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MySfgPetClinic2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
